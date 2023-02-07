2a90b3bfb455bb807c38c7cafa8f46a14ff30761
Evan_Boswell <Evanckboswell@gmail.com>
2023-02-07 03:58:36 -0800

    Added Images to buttons

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,12 @@ function App() {
                 <span>Biology Research Engine</span>
                 <div className={styles['Bottom-Header']}>
                     <button className={styles['Home-Button']}>
-                        <img src="./styles/Home_Button.svg" />
+                        <img
+                            id="home-button-img"
+                            src="/styles/Home_Button.svg"
+                            alt="Home"
+                            className={styles.HomeButtonImage}
+                        />
                     </button>
                     <input className={styles['Search-Bar']} />
                     <select className={styles['Database-Selection']}>
@@ -19,7 +24,14 @@ function App() {
                         <option>Amphibians</option>
                         <option>Birds</option>
                     </select>
-                    <button className={styles['Search-Button']}></button>
+                    <button className={styles['Search-Button']}>
+                        <img
+                            id="search-button-img"
+                            src="/styles/Search_Button.svg"
+                            alt="Search"
+                            className={styles.SearchButtomImage}
+                        />
+                    </button>
                 </div>
             </header>
         </div>
