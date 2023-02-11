f50d4b4a95247d646e82ee0f125d269dd63194ad
Evan_Boswell <Evanckboswell@gmail.com>
2023-02-10 20:22:14 -0800

    GG-77 Made the header its own separate component from the main app

--- src/_codux/boards/app/app.board.tsx
+++ src/_codux/boards/app/app.board.tsx
@@ -2,7 +2,7 @@ import { createBoard } from '@wixc3/react-board';
 import App from '../../../App';
 
 export default createBoard({
-    name: 'Header',
+    name: 'Home',
     Board: () => <App></App>,
     environmentProps: {
         windowWidth: 862,
