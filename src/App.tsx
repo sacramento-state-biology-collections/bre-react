99137629983f46706cacc0c9817724291cfbba83
Almaguer, Brian <balmaguer@csus.edu>
2023-02-14 19:41:10 -0800

    fixed tsx issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import { WelcomePage } from './components/welcome-page/welcome-page';
+import { LoginRibbon } from './components/login-ribbon/login-ribbon';
+
 function App() {
-    return <div></div>;
+    return (
+        <div>
+            <LoginRibbon />
+            <WelcomePage />
+        </div>
+    );
 }
 
 export default App;
