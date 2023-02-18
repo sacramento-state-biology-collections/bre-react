3ede4ea5b8702783a85e52b94fd5dabe4e32b99e
Lauren Prather <lprather@csus.edu>
2023-02-17 18:04:21 -0800

    temp

--- src/_codux/boards/table-data-view/table-data-view.board.tsx
+++ src/_codux/boards/table-data-view/table-data-view.board.tsx
@@ -4,56 +4,8 @@ import TableDataView_module from '../../../components/table-data-view/table-data
 
 export default createBoard({
     name: 'TableDataView',
-    Board: () => (
-        <TableDataView className={TableDataView_module.root}>
-            <table className={TableDataView_module.TableMainClass}>
-                <li className={TableDataView_module.PlaceholderItems}>Common Name</li>
-                <li className={TableDataView_module.PlaceholderItems}>Scientific Name</li>
-                <li className={TableDataView_module.PlaceholderItems}>Prep Type</li>
-                <li className={TableDataView_module.PlaceholderItems}>Drawer #</li>
-                <li className={TableDataView_module.PlaceholderItems}> </li>
-            </table>
-            <tr className={TableDataView_module.TableRow}>
-                <li className={TableDataView_module.TableRowItem}>Item</li>
-                <li className={TableDataView_module.TableRowItem}>Item</li>
-                <li className={TableDataView_module.TableRowItem}>Item</li>
-                <li className={TableDataView_module.TableRowItem}>Item</li>
-                <li className={TableDataView_module.TableRowItem}>
-                    <button className={TableDataView_module.PreviewButtonStyles}>Preview</button>
-                </li>
-            </tr>
-            <tr className={TableDataView_module.TableRow}>
-                <li className={TableDataView_module.TableRowItem}>Item</li>
-                <li className={TableDataView_module.TableRowItem}>Item</li>
-                <li className={TableDataView_module.TableRowItem}>Item</li>
-                <li className={TableDataView_module.TableRowItem}>Item</li>
-                <li className={TableDataView_module.TableRowItem}>
-                    <button className={TableDataView_module.PreviewButtonStyles}>Preview</button>
-                </li>
-            </tr>
-            <tr className={TableDataView_module.TableRow}>
-                <li className={TableDataView_module.TableRowItem}>Item</li>
-                <li className={TableDataView_module.TableRowItem}>Item</li>
-                <li className={TableDataView_module.TableRowItem}>Item</li>
-                <li className={TableDataView_module.TableRowItem}>Item</li>
-                <li className={TableDataView_module.TableRowItem}>
-                    <button className={TableDataView_module.PreviewButtonStyles}>Preview</button>
-                </li>
-            </tr>
-            <tr className={TableDataView_module.TableRow}>
-                <li className={TableDataView_module.TableRowItem}>Item</li>
-                <li className={TableDataView_module.TableRowItem}>Item</li>
-                <li className={TableDataView_module.TableRowItem}>Item</li>
-                <li className={TableDataView_module.TableRowItem}>Item</li>
-                <li className={TableDataView_module.TableRowItem}>
-                    <button className={TableDataView_module.PreviewButtonStyles}>Preview</button>
-                </li>
-            </tr>
-        </TableDataView>
-    ),
+    Board: () => <TableDataView className={TableDataView_module.root}></TableDataView>,
     environmentProps: {
-        canvasHeight: 870,
-        canvasWidth: 1000,
-        windowWidth: 1024,
+        canvasWidth: 1030,
     },
 });
--- src/components/table-data-view/table-data-view.tsx
+++ src/components/table-data-view/table-data-view.tsx
@@ -1,5 +1,6 @@
 import styles from './table-data-view.module.scss';
 import classNames from 'classnames';
+import { useEffect, useState } from 'react';
 
 export interface TableDataViewProps {
     className?: string;
@@ -10,6 +11,38 @@ export interface TableDataViewProps {
  * This component was generated using Codux's built-in Default new component template.
  * For details on how to create custom new component templates, see https://help.codux.com/kb/en/article/configuration-for-table-data-views-and-templates
  */
+
 export const TableDataView = ({ className, children = 'TableDataView' }: TableDataViewProps) => {
-    return <div className={classNames(styles.root, className)}>{children}</div>;
+    const [data, setData] = useState([]);
+
+    const getTable = async () => {
+        const tableData = await fetch("50.116.3.37:9001", 
+        {method: "GET", });
+        const jsonData = await tableData.json();
+        setData(jsonData);
+        console.log(data);
+    }
+
+    useEffect(() => {
+        getTable();
+    }, []);
+
+    return <div className={classNames(styles.root, className)}>
+        <table className={styles.TableMainClass}>
+                <li className={styles.PlaceholderItems}>Common Name</li>
+                <li className={styles.PlaceholderItems}>Scientific Name</li>
+                <li className={styles.PlaceholderItems}>Prep Type</li>
+                <li className={styles.PlaceholderItems}>Drawer #</li>
+                <li className={styles.PlaceholderItems}> </li>
+            </table>
+            <tr className={styles.TableRow}>
+                <li className={styles.TableRowItem}>Item</li>
+                <li className={styles.TableRowItem}>Item</li>
+                <li className={styles.TableRowItem}>Item</li>
+                <li className={styles.TableRowItem}>Item</li>
+                <li className={styles.TableRowItem}>
+                    <button className={styles.PreviewButtonStyles}>Preview</button>
+                </li>
+            </tr>
+    </div>;
 };
