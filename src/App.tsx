45ad0d64fa3e18f5ea61a32728d2f746dad41997
jo3y49 <71416767+jo3y49@users.noreply.github.com>
2023-02-26 17:09:52 -0800

    cleaned up the code

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
 import { useState } from 'react';
 import { WelcomeView } from './components/welcome-view/welcome-view';
-import { DemoView } from './components/demo-view/demo-view';
 import { BREHomeView } from './components/bre-home-view/bre-home-view';
-import { Intro } from './components/intro/intro';
 import { IntroView } from './components/intro-view/intro-view';
 
 function App() {
-    const views = ["welcome-view", ]
-
     const [currentView, setView] = useState(0);
 
     function toggleView(nextView: number) {
