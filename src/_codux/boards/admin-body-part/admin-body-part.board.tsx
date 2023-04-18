b9dc368f2bfdcbf8e2a6b636f3652fe6aad5568e
May, Joseph <josephmay@csus.edu>
2023-04-17 18:29:55 -0700

    deleted outdated boards and components

--- src/_codux/boards/admin-body-part/admin-body-part.board.tsx
+++
@@ -1,7 +0,0 @@
-import { createBoard } from '@wixc3/react-board';
-import { Admin_Body_Part } from '../../../components/admin-body-part/admin-body-part';
-
-export default createBoard({
-    name: 'Admin_Body_Part',
-    Board: () => <Admin_Body_Part toggle_AdminPanelView={()=>{}}/>
-});
